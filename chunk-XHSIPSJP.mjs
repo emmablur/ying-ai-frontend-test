041f8c784bf59a163035939936adaef19858f447
emmablur <emmablur@users.noreply.github.com>
2025-06-04 12:24:19 +0000

    deploy: emmablur/ying-ai-frontend@b8729a1449ed92458a146c58fd698c0ca9563907

---
+++ chunk-XHSIPSJP.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{q as m,w as p}from"./chunk-3N7YS55T.mjs";import{Mb as n,Nc as a,W as t,nb as r}from"./chunk-MABGOXGD.mjs";import"./chunk-M3IHTRDF.mjs";var i=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-order"]],standalone:!0,features:[n],decls:1,vars:0,template:function(e,c){e&1&&r(0,"router-outlet")},dependencies:[p,m,a],encapsulation:2})};export{i as OrderComponent};
